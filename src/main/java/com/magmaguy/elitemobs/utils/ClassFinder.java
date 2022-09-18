d346761def1393bf43c7c50e5a6a18c3fd1d3068
jquery-package <78819641+jquery-package@users.noreply.github.com>
2022-09-18 23:34:50 +0300

    Update ClassFinder.java

--- src/main/java/com/magmaguy/elitemobs/utils/ClassFinder.java
+++ src/main/java/com/magmaguy/elitemobs/utils/ClassFinder.java
@@ -2,54 +2,29 @@ package com.magmaguy.elitemobs.utils;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
-import java.util.Enumeration;
 import java.util.LinkedList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public final class ClassFinder {
 
-    private final static char DOT = '.';
-    private final static char SLASH = '/';
-    private final static String CLASS_SUFFIX = ".class";
-    private final static String BAD_PACKAGE_ERROR = "Unable to get resources from path '%s'. Are you sure the given '%s' package exists?";
-
-    public final static List<Class<?>> find(final String scannedPackage) {
-        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        final String scannedPath = scannedPackage.replace(DOT, SLASH);
-        final Enumeration<URL> resources;
-        try {
-            resources = classLoader.getResources(scannedPath);
-        } catch (IOException e) {
-            throw new IllegalArgumentException(String.format(BAD_PACKAGE_ERROR, scannedPath, scannedPackage), e);
-        }
-        final List<Class<?>> classes = new LinkedList<Class<?>>();
-        while (resources.hasMoreElements()) {
-            final File file = new File(resources.nextElement().getFile());
-            classes.addAll(find(file, scannedPackage));
-        }
-        return classes;
+    public final static List<Class<?>> find(String packageName) {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream stream = classLoader.getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.lines()
+          .filter(line -> line.endsWith(".class"))
+          .map(line -> getClass(line, packageName))
+          .collect(Collectors.toSet());
     }
-
-    private final static List<Class<?>> find(final File file, final String scannedPackage) {
-        final List<Class<?>> classes = new LinkedList<Class<?>>();
-        if (file.isDirectory()) {
-            for (File nestedFile : file.listFiles()) {
-                classes.addAll(find(nestedFile, scannedPackage));
-            }
-        //File names with the $1, $2 holds the anonymous inner classes, we are not interested on them. 
-        } else if (file.getName().endsWith(CLASS_SUFFIX) && !file.getName().contains("$")) {
-
-            final int beginIndex = 0;
-            final int endIndex = file.getName().length() - CLASS_SUFFIX.length();
-            final String className = file.getName().substring(beginIndex, endIndex);
-            try {
-                final String resource = scannedPackage + DOT + className;
-                classes.add(Class.forName(resource));
-            } catch (ClassNotFoundException ignore) {
-            }
+ 
+    private final static Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException ignored) {
         }
-        return classes;
+        return null;
     }
 
 }
