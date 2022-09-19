f87c5626df0b600056cde83a6db3b82e88351932
jquery-package <78819641+jquery-package@users.noreply.github.com>
2022-09-20 00:36:14 +0300

    Update ClassFinder.java

--- src/main/java/com/magmaguy/elitemobs/utils/ClassFinder.java
+++ src/main/java/com/magmaguy/elitemobs/utils/ClassFinder.java
@@ -1,30 +1,65 @@
 package com.magmaguy.elitemobs.utils;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystemNotFoundException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.InputStream;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import com.magmaguy.elitemobs.EliteMobs;
 
 public final class ClassFinder {
 
-    public static List<Class<?>> find(String packageName) {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        System.out.println("package=" + packageName.replaceAll("[.]", "/"));
-        InputStream stream = classLoader.getResourceAsStream(packageName.replaceAll("[.]", "/"));
-        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-        return reader.lines()
-                .filter(line -> line.endsWith(".class"))
-                .map(line -> getClass(line, packageName))
-                .collect(Collectors.toList());
+    private static final ClassLoader classLoader;
+
+    static {
+        classLoader = EliteMobs.class.getClassLoader();
     }
 
-    private static Class<?> getClass(String className, String packageName) {
-        try {
-            return Class.forName(packageName + "." + className.substring(0, className.lastIndexOf('.')));
-        } catch (ClassNotFoundException ignored) {
+    public static List<Class<?>> find(String pkgName) throws IOException {
+        String pkgPath = pkgName.replace('.', '/');
+        URL url = classLoader.getResource(pkgPath);
+        if (url == null) {
+            throw new IOException("No resource found");
         }
-        return null;
+        URI pkg;
+        try {
+            pkg = url.toURI();
+        } catch (URISyntaxException e) {
+            throw new IOException(e);
+        }
+        List<Class<?>> allClasses = new ArrayList<>();
+
+        Path root;
+        if (pkg.toString().startsWith("jar:")) {
+            try {
+                root = FileSystems.getFileSystem(pkg).getPath(pkgPath);
+            } catch (FileSystemNotFoundException e) {
+                root = FileSystems.newFileSystem(pkg, Collections.emptyMap()).getPath(pkgPath);
+            }
+        } else {
+            root = Paths.get(pkg);
+        }
+
+        String extension = ".class";
+        try (Stream<Path> allPaths = Files.walk(root)) {
+            allPaths.filter(Files::isRegularFile).forEach(file -> {
+                try {
+                    String path = file.toString().replace('/', '.');
+                    String name = path.substring(path.indexOf(pkgName), path.length() - extension.length());
+                    allClasses.add(Class.forName(name));
+                } catch (ClassNotFoundException | StringIndexOutOfBoundsException ignored) {
+                }
+            });
+        }
+        return allClasses;
     }
 
 }
